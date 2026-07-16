f43f8cb5454d14d52b98c6d5d16459129e3d970c
DuyKhoiCoder30062004 <anhkhoi.bo@gmail.com>
2026-07-16 19:59:27 +0700

    start

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,7 @@
 import { createStart, createMiddleware } from "@tanstack/react-start";
 
 import { renderErrorPage } from "./lib/error-page";
-
+// qwewq
 const errorMiddleware = createMiddleware().server(async ({ next }) => {
   try {
     return await next();
